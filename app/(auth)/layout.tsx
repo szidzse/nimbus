1baffacf76ec299be28b2e5f04818b672c80dfb3
Sós Gellért <gellertsos@gmail.com>
2024-12-23 17:37:24 +0100

    implement design for layout right side

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -29,7 +29,19 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
           />
         </div>
       </section>
-      {children}
+
+      <section className="flex flex-1 flex-col items-center bg-white p-4 py-10 lg:justify-center lg:p-10 lg:py-0">
+        <div className="mb-16 lg:hidden">
+          <Image
+            src="/assets/icons/logo-full.svg"
+            alt="logo"
+            width={224}
+            height={82}
+            className="h-auto w-[200px] lg:w-[250px]"
+          />
+        </div>
+        {children}
+      </section>
     </div>
   );
 };
