1d2e909b8be1ca4f2ce03b5e27b7728bcf33b391
Sós Gellért <gellertsos@gmail.com>
2024-12-23 13:04:29 +0100

    set utility classes in globals.css

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
 export default function Home() {
   return (
-    <div className="flex items-center justify-center h-screen bg-amber-100">
-      <h1 className="text-3xl font-bold text-brand">
-        Nimbus - Cloud storage made simple.
-      </h1>
+    <div className="flex-center h-screen">
+      <h1 className="h1">Nimbus - Cloud storage made simple.</h1>
     </div>
   );
 }
