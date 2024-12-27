0ac1bcdac482b85d0049c48d2251980b982d1eb1
Sós Gellért <gellertsos@gmail.com>
2024-12-27 16:29:02 +0100

    install shadcn and create basic reusable form

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import AuthForm from "@/components/AuthForm";
 
 const SignIn = () => {
-  return <div>Sign In</div>;
+  return <AuthForm type="sign-in" />;
 };
 
 export default SignIn;
--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import AuthForm from "@/components/AuthForm";
 
 const SignUp = () => {
-  return <div>Sign Up</div>;
+  return <AuthForm type="sign-up" />;
 };
 
 export default SignUp;
