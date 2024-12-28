4a690a0c00bde7dda46c9f859527576b65b044fc
Sós Gellért <gellertsos@gmail.com>
2024-12-28 15:46:11 +0100

    minor change in auth form

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -149,7 +149,7 @@ const AuthForm = ({ type }: { type: FormType }) => {
         </form>
       </Form>
 
-      {true && (
+      {accountId && (
         <OTPModal email={form.getValues("email")} accountId={accountId} />
       )}
     </>
