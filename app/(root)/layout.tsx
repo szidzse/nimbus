3990c58e3f35620415f75f368229d1a3506eebc7
Sós Gellért <gellertsos@gmail.com>
2024-12-28 20:28:08 +0100

    create Sidebar MobileNavigation and Header component and import them in root layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,15 @@
 import React from "react";
+import Sidebar from "@/components/Sidebar";
+import MobileNavigation from "@/components/MobileNavigation";
+import Header from "@/components/Header";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="flex h-screen">
-      Sidebar
+      <Sidebar />
       <section className="flex h-full flex-1 flex-col">
-        MobileNavigation Header
+        <MobileNavigation />
+        <Header />
         <div className="main-content">{children}</div>
       </section>
     </main>
