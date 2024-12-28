c1bbaf00ec2885a44a0792be3dbbdb76cd7cf68f
Sós Gellért <gellertsos@gmail.com>
2024-12-28 11:33:52 +0100

    create index.ts to use Appwrite API via node sdk

---
+++ lib/appwrite/index.ts
@@ -0,0 +1,57 @@
+/*
+This file uses the Node.js SDK to interact with Appwrite, a backend platform for features like authentication, database management, file storage, and more.
+It defines two clients:
+ -User Client: Authenticates via session cookies for user-specific actions.
+ -Admin Client: Uses an admin key for full access to Appwrite services.
+
+  https://www.npmjs.com/package/node-appwrite
+*/
+
+import { Account, Avatars, Client, Databases, Storage } from "node-appwrite";
+import { appwriteConfig } from "@/lib/appwrite/config";
+import { cookies } from "next/headers";
+
+export const createSessionClient = async () => {
+  const client = new Client()
+    .setEndpoint(appwriteConfig.endpointUrl)
+    .setProject(appwriteConfig.projectId);
+
+  const session = (await cookies()).get("appwrite-session");
+
+  if (!session || !session.value) {
+    throw new Error("No session.");
+  }
+
+  client.setSession(session.value);
+
+  return {
+    get account() {
+      return new Account(client);
+    },
+    get databases() {
+      return new Databases(client);
+    },
+  };
+};
+
+export const createAdminClient = async () => {
+  const client = new Client()
+    .setEndpoint(appwriteConfig.endpointUrl)
+    .setProject(appwriteConfig.projectId)
+    .setKey(appwriteConfig.secretKey);
+
+  return {
+    get account() {
+      return new Account(client);
+    },
+    get databases() {
+      return new Databases(client);
+    },
+    get storage() {
+      return new Storage(client);
+    },
+    get avatars() {
+      return new Avatars(client);
+    },
+  };
+};
