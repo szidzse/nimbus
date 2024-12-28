b4f130af04a8adffb1d529453abfe31fd567690a
Sós Gellért <gellertsos@gmail.com>
2024-12-28 20:43:42 +0100

    start implementing Sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,7 +1,29 @@
 import React from "react";
+import Link from "next/link";
+import Image from "next/image";
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return (
+    <aside className="sidebar">
+      <Link href="/">
+        <Image
+          src="/assets/icons/logo-full.svg"
+          alt="logo"
+          width={160}
+          height={50}
+          className="hidden h-auto lg:block"
+        />
+
+        <Image
+          src="/assets/icons/logo-full.svg"
+          alt="logo"
+          width={52}
+          height={52}
+          className="lg:hidden"
+        />
+      </Link>
+    </aside>
+  );
 };
 
 export default Sidebar;
