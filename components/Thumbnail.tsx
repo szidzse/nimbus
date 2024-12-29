5e422fc0dcbc505b9cd7baf44dcf382b1e5649e3
Sós Gellért <gellertsos@gmail.com>
2024-12-29 17:30:12 +0100

    finish Thumbnail to display something based on file extension

--- components/Thumbnail.tsx
+++ components/Thumbnail.tsx
@@ -1,19 +1,37 @@
 import React from "react";
 import Image from "next/image";
-import { getFileIcon } from "@/lib/utils";
+import { cn, getFileIcon } from "@/lib/utils";
 
 interface Props {
   type: string;
   extension: string;
   url?: string;
+  imageClassName?: string;
+  className?: string;
 }
 
-const Thumbnail = ({ type, extension, url = "" }: Props) => {
+const Thumbnail = ({
+  type,
+  extension,
+  url = "",
+  imageClassName,
+  className,
+}: Props) => {
   const isImage = type === "image" && extension !== "svg";
 
   return (
-    <figure>
-      <Image src={isImage ? url : getFileIcon(extension, type)} alt="" />
+    <figure className={cn("thumbnail", className)}>
+      <Image
+        src={isImage ? url : getFileIcon(extension, type)}
+        alt="Thumbnail"
+        width={100}
+        height={100}
+        className={cn(
+          "size-8 object-contain",
+          imageClassName,
+          isImage && "thumbnail-image",
+        )}
+      />
     </figure>
   );
 };
