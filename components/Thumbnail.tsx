333860443207d4d2732e01468b59ff2b417309ba
Sós Gellért <gellertsos@gmail.com>
2024-12-29 17:17:15 +0100

    implement getFileIcon fn

--- components/Thumbnail.tsx
+++ components/Thumbnail.tsx
@@ -1,7 +1,21 @@
 import React from "react";
+import Image from "next/image";
+import { getFileIcon } from "@/lib/utils";
 
-const Thumbnail = () => {
-  return <div>Thumbnail</div>;
+interface Props {
+  type: string;
+  extension: string;
+  url?: string;
+}
+
+const Thumbnail = ({ type, extension, url = "" }: Props) => {
+  const isImage = type === "image" && extension !== "svg";
+
+  return (
+    <figure>
+      <Image src={isImage ? url : getFileIcon(extension, type)} alt="" />
+    </figure>
+  );
 };
 
 export default Thumbnail;
