50e8a36a3aef2fc2a6523390bc2559092d1ad359
Sós Gellért <gellertsos@gmail.com>
2025-01-01 14:37:17 +0100

    create dynamic route

---
+++ app/(root)/[type]/page.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { SearchParamProps } from "@/types";
+
+const Page = async ({ params }: SearchParamProps) => {
+  const type = ((await params)?.type as string) || "";
+
+  return (
+    <div className="page-container">
+      <section className="w-full">
+        <h1 className="h1 capitalize">{type}</h1>
+
+        <div className="total-size-section">
+          <p className="body-1">
+            Total: <span className="h5">0 MB</span>
+          </p>
+
+          <div className="sort-container">
+            <p className="body-1 hidden text-light-200 sm:block">Sort by: </p>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Page;
--- types/index.d.ts
+++ types/index.d.ts
@@ -1,3 +1,8 @@
+export interface SearchParamProps {
+  params?: Promise<SegmentParams>;
+  searchParams?: Promise<{ [key: string]: string | string[] | undefined }>;
+}
+
 export interface UploadFileProps {
   file: File;
   ownerId: string;
